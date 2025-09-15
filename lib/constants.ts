f3e09739f40f1a40c3e30bc86426147ff5d157ab
ktan-wolf <ktan247610@gmail.com>
2025-09-15 09:13:50 +0000

    deregister updated

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,11 +3,11 @@ import { PublicKey } from "@solana/web3.js";
 
 // Your program ID
 export const PROGRAM_ID = new PublicKey(
-  "HQvqX5DpDonJN2YjshaTSrRRJmZvBe3fhYGy7aSvceuT"
+  "3je23jfTQJBkYTYhLCBjH2F9thAcaY9g7M7RYR92uhWu"
 );
 
 // The mint address of the token you want to use for staking.
 // 🚨 IMPORTANT: Replace this with the actual mint address from your localnet setup.
 export const MINT_ADDRESS = new PublicKey(
-  "99Y6T91yfEUmbyXJunB8giqR9idFydBCxASPRekLFJVF" // e.g. "4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDUckr"
+  "CDNu1CuzFnQ5kadsBQixii3mocTHDpCcTdch9hHNwd9s" // e.g. "4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDUckr"
 );
